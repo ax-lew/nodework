51ef8717153b56c9242714b0ff06d9c6e7d036ce
alew <axel.lew@hotmail.com>
2016-12-01 12:18:47 -0300

    asd

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ var http = require("http");
 var server = http.createServer();
 
 server.on("request", function(req,res){
+	res.write(JSON.stringify(getDateNow()) + "\n");
 	res.end("Hola, mundo");
-
-
 })
 
 server.listen(8080);
+
+function getDateNow(){
+	var now = new Date();
+	return now;
+}
