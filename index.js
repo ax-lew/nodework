a4ab454c92cd27fd8a27f13a418fb8d3660841ec
alew <axel.lew@hotmail.com>
2016-12-01 14:45:39 -0300

    asd

--- index.js
+++ index.js
@@ -4,31 +4,22 @@ var fs = require("fs");
 
 var server = http.createServer();
 
-server.on("request", function(req,res){
-	res.write(JSON.stringify(getDateNow()) + "\n");
-	var urlData = url.parse(req.url,true);
-	var path = urlData["pathname"];
-	
-	fs.readdir("../public/", function(err, items) {
-		//console.log(items);
-		for (var i=0; i<items.length; i++) {
-			if(path == "/" + items[i]){
-				//console.log("Anda" + "\n");
-				res.write("Anda" + "\n");
-			}
-			
-		}
-	});
-
-	res.write(JSON.stringify(path) + "\n");
-	res.end("Hola, mundo");
-})
-
-
-
 server.listen(process.env.PORT || 3000);
 
 function getDateNow(){
 	var now = new Date();
 	return now;
 }
+
+server.on("request", function(req,res){
+	res.write(JSON.stringify(getDateNow()) + "\n");
+	var urlData = url.parse(req.url,true);
+	var path = urlData["pathname"];
+	
+	
+
+	res.write(JSON.stringify(path) + "\n");
+	res.end("Hola, mundo");
+})
+
+
