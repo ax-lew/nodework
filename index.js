eab957cca0549ad4ce23d36fe2e1568e5d360d6d
alew <axel.lew@hotmail.com>
2016-12-02 10:28:27 -0300

    asd

--- index.js
+++ index.js
@@ -11,15 +11,51 @@ function getDateNow(){
 	return now;
 }
 
+
+function leerArchivo(path,res){
+	fs.readFile(path, function(err,data){
+		if (err){
+			res.writeHead(500);
+			res.end("Algo salio mal");
+		} else{
+			res.write(data);
+			res.end();
+		}
+	})
+}
+
+function error404(res){
+	fs.readFile("img/Error404.png", function(err,data){
+		if (err){
+			res.writeHead(500);
+			res.end("Algo salio mal");
+		} else{
+			res.write(data);
+			res.end();
+		}
+	})
+}
+
 server.on("request", function(req,res){
-	res.write(JSON.stringify(getDateNow()) + "\n");
+	//res.write(JSON.stringify(getDateNow()) + "\n");
 	var urlData = url.parse(req.url,true);
-	var path = urlData["pathname"];
+	var path = urlData.pathname;
 	
+	if (path != "/favicon.ico"){
+		fs.exists("../public" + path, function (exist) {
+			if (exist) {
+				leerArchivo("../public" + path,res);
+			} else {
+				error404(res);
+			}
+			
+		})
+	}
+
 	
 
-	res.write(JSON.stringify(path) + "\n");
-	res.end("Hola, mundo");
+	//res.write(JSON.stringify(path) + "\n");
+	//res.end("Hola, mundo");
 })
 
 
